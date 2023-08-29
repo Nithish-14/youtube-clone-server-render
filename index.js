5cb6a1eae4c46f8e505bcd76fbdda8035f991b09
Nithish <nithishm612345@gmail.com>
2023-08-29 17:51:37 +0530

    isFinal

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const authenticator = (request, response, next) => {
 
 //Test API
 app.get("/", (request, response) => {
-    response.send("Success")
+    response.send("Successful")
 })
 
 //Create User API
