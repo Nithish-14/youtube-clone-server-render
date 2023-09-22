1b10af3f8bd9b0d409679e03021b0bd2f6deec3f
Nithish <nithishm612345@gmail.com>
2023-09-22 11:53:49 +0530

    f cors

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const dbPath = path.join(__dirname, "youtubeClone.db");
 
 //Using middleware functions
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://youtube-clone-nithish-mnl.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 let db = null;
 
